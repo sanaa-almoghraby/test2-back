822580cf87840e75ad36f0f4f806f50b100e711d
sanaa-almoghraby <sanaa.almoghraby@gmail.com>
2021-08-23 22:24:06 +0300

    save

--- server.js
+++ server.js
@@ -4,13 +4,9 @@ const express = require('express');
 const cors = require('cors');
 const server = express();
 require('dotenv').config();
-const PORT =process.env.PORT //3004
+const PORT = process.env.PORT //3004
 server.use(cors());
 server.use(express.json());
-const getApidata =require('./model/GetApidata')
-
-// http://localhost:3004/
-server.get('/', testfun)
 
 const mongoose = require('mongoose');
 const { default: axios } = require('axios');
@@ -20,193 +16,136 @@ const { default: axios } = require('axios');
 // mongodb://sanaa:<password>@cluster0-shard-00-00.ejhje.mongodb.net:27017,cluster0-shard-00-01.ejhje.mongodb.net:27017,cluster0-shard-00-02.ejhje.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-10qf27-shard-0&authSource=admin&retryWrites=true&w=majority
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const dragonSchema = new mongoose.Schema({
+const pokemonSchema = new mongoose.Schema({
     name: String,
     img: String,
     level: String,
 });
-const wonerSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     email: String,
-    bokemon: [dragonSchema]
+    data: [pokemonSchema]
 });
-
-const user = mongoose.model('user', wonerSchema);
-
-function seedData() {
+const user = mongoose.model('user', userSchema);
+function seedfun() {
     const userdata = new user({
         email: 'sanaa.almoghraby@gmail.com',
-        bokemon: [{
-            "name": "Koromon",
-            "img": "https://digimon.shadowsmith.com/img/koromon.jpg",
-            "level": "In Training"
-        },
-        {
-            "name": "Tsunomon",
-            "img": "https://digimon.shadowsmith.com/img/tsunomon.jpg",
-            "level": "In Training"
-        },
-        {
-            "name": "Yokomon",
-            "img": "https://digimon.shadowsmith.com/img/yokomon.jpg",
-            "level": "In Training"
-        },]
-    })
-
-    const userdata2 = new user({
-        email: 'sanaa@gmail.com',
-        bokemon: [{
-            "name": "Koromon",
-            "img": "https://digimon.shadowsmith.com/img/koromon.jpg",
-            "level": "In Training"
-        },
-        {
-            "name": "Tsunomon",
-            "img": "https://digimon.shadowsmith.com/img/tsunomon.jpg",
-            "level": "In Training"
-        },
-        {
-            "name": "Yokomon",
-            "img": "https://digimon.shadowsmith.com/img/yokomon.jpg",
-            "level": "In Training"
-        },]
+        data: [
+            {
+                "name": "Koromon",
+                "img": "https://digimon.shadowsmith.com/img/koromon.jpg",
+                "level": "In Training"
+            },
+            {
+                "name": "Tsunomon",
+                "img": "https://digimon.shadowsmith.com/img/tsunomon.jpg",
+                "level": "In Training"
+            },
+            {
+                "name": "Yokomon",
+                "img": "https://digimon.shadowsmith.com/img/yokomon.jpg",
+                "level": "In Training"
+            },]
     })
     userdata.save();
-    userdata2.save();
 }
-// seedData();
+// seedfun();
 
-
-
-
-// https://digimon-api.vercel.app/api/digimon
-server.get('/api/digimon', getApidata)
-
-// http://localhost:3004/addtofav
-server.post('/addtofav', addtoFavFun)
-
-
-// http://localhost:3004/update/index
-server.put('/update/:id', updatefun)
-
-
-
-// http://localhost:3004/delete/index
-
-server.delete('/delete/:id', deletedata)
-
-
-
-function deletedata(req, res) {
-    let id =Number(req.params.id) 
-    let email = req.query.email
-    user.findOne({ email: email }, (err, deleteddata) => {
-        // deleteddata.bokemon.splice(id,1);
-
-
-        if (err) {
-            res.send('error')
-        } else {
-            let newdeledata = deleteddata.bokemon.filter((el, ind) => {
-                if (ind !== id) {
-                    return el;
-                }
-            })
-            deleteddata.bokemon = newdeledata;
-
-            deleteddata.save();
-            res.send( deleteddata.bokemon)
-        }
-    })
-}
-
-function updatefun(req, res) {
-    const { email, name, img, level } = req.body
-    let index = Number(req.params.id)
-
-    user.findOne({ email: email }, (err, updetedata) => {
-        if (err) {
-            res.send('error')
-
-        } else {
-            updetedata.bokemon.splice(index, 1, {
-                name: name,
-                img: img,
-                level: level
-            })
-        }
-        updetedata.save();
-        res.send(updetedata.bokemon);
-    })
-
-}
-
-
-function addtoFavFun(req, res) {
-    const { email, name, img, level } = req.body
-    user.find({ email: email }, (err, favData) => {
-        if (err) {
-            res.send('error')
-        } else {
-            const newfav = {
-                name: name,
-                img: img,
-                level: level
-            }
-            favData[0].bokemon.push(newfav)
-
-        }
-        favData[0].save();
-        res.send(favData[0].bokemon)
-    })
-
-}
-
-// async function getApidata(req, res) {
-//     let apiData = await axios.get('https://digimon-api.vercel.app/api/digimon')
-
-//     let allDAta = apiData.data.map(ele => {
-//         return new GetdataApi(ele)
-//     })
-//     res.send(allDAta)
-// }
-// class GetdataApi {
-//     constructor(data) {
-//         this.name = data.name,
-//             this.img = data.img,
-//             this.level = data.level
-//     }
-
-// }
-
-// =============================================================================
-// http://localhost:3004/alldata?email=
-server.get('/alldata', getdata)
-
+// http://localhost:3004/getdatadb?email=sanaa.almoghraby@gmail.com
+server.get('/getdatadb', getdata)
 function getdata(req, res) {
     let email = req.query.email
-    user.find({ email: email }, (err, cartondata) => {
+    user.find({ email: email }, (err, databok) => {
         if (err) {
-            res.send('not correct')
+            res.send('erorrrrr')
         } else {
-            res.send(cartondata[0].bokemon)
+            res.send(databok[0].data)
         }
-        console.log(cartondata);
+    })
+}
+// http://localhost:3004/dataapi
+server.get('/dataapi', dataApi)
+
+function dataApi(req, res) {
+    let urlApi = 'https://digimon-api.vercel.app/api/digimon';
+    axios.get(urlApi).then(item => {
+        let dataApi = item.data.map(ele => {
+            return new newopj(ele)
+        })
+        res.send(dataApi)
+    })
+}
+
+class newopj {
+    constructor(alldata) {
+        this.name = alldata.name;
+        this.img = alldata.img;
+        this.level = alldata.level;
+
+    }
+}
+
+// http://localhost:3004/addtofav
+server.post('/addtofav', addtofavfun)
+
+function addtofavfun(req, res) {
+    const { email, name, img, level } = req.body;
+    user.find({ email: email }, (err, databok) => {
+        if (err) {
+            res.send('erorrrrr')
+        } else {
+            const newopj = {
+                name: name,
+                img: img,
+                level: level,
+            }
+            databok[0].data.push(newopj)
+        }
+        databok[0].save();
+        res.send(databok[0]);
     })
 
 }
+// http://localhost:3004/delete/inx
+server.delete('/delete/:id', deletefun)
 
+function deletefun(req, res) {
+    let email = req.query.email
+    let ind = req.params.id
+    user.findOne({ email: email }, (err, databok) => {
+        if (err) {
+            res.send('erorrrrr')
+        } else {
+            databok.data.splice(ind, 1);
+            databok.save();
+            res.send(databok.data)
+        }
+    })
 
-
-
-
-
-function testfun(req, res) {
-
-    console.log('goooood');
-    res.send('work good ssssssssss')
+}
+// http://localhost:3004/update/inx
+server.put('/update/:id', updatefun)
+function updatefun(req, res) {
+    let { email, name, img, level } = req.body;
+    let ind = req.params.id
+    user.findOne({ email: email }, (err, databok) => {
+        if (err) {
+            res.send('erorrrrr')
+        } else {
+            databok.data.splice(ind, 1, {
+                name: name,
+                img: img,
+                level: level,
+            });
+            databok.save();
+            res.send(databok.data)
+        }
+    })
 }
 
+
+// // http://localhost:3004/
+// server.get('/', testfun)
+// function testfun
 
 server.listen(PORT, () => {
     console.log(`listen on ${PORT}`);
